d389b3b03915906aacd6c95390b77390650d3658
Luiz Silveira <zertyz@gmail.com>
2020-03-18 23:04:54 -0300

    More planning...

--- docs/Event Model.mm
+++ docs/Event Model.mm
@@ -89,10 +89,15 @@
 <edge COLOR="#ffffff"/>
 <node TEXT="CommunicationPatterns" POSITION="right" ID="ID_273436263" CREATED="1584490898039" MODIFIED="1584498689854" HGAP_QUANTITY="91.26829627750192 pt" VSHIFT_QUANTITY="-30.731708746733723 pt">
 <edge COLOR="#007c7c"/>
-<node TEXT="Producer(s) / Consumer(s)" ID="ID_588194298" CREATED="1584490898041" MODIFIED="1584498626226" HGAP_QUANTITY="15.756097642670499 pt" VSHIFT_QUANTITY="-12.292683498693489 pt"/>
-<node TEXT="Broadcast" ID="ID_1104103844" CREATED="1584490898041" MODIFIED="1584498593816"/>
-<node TEXT="RPC" ID="ID_849688082" CREATED="1584498594625" MODIFIED="1584498599218"/>
-<node ID="ID_734055308" CREATED="1584499817834" MODIFIED="1584499831055"><richcontent TYPE="NODE">
+<node TEXT="Entities" ID="ID_1990223157" CREATED="1584580990004" MODIFIED="1584582256949">
+<font ITALIC="true"/>
+<node TEXT="Producer" ID="ID_1020210804" CREATED="1584580999505" MODIFIED="1584581847492">
+<font BOLD="true"/>
+<node TEXT="generate the events" ID="ID_1048539396" CREATED="1584581032297" MODIFIED="1584581042722"/>
+</node>
+<node TEXT="Consumer" ID="ID_878415291" CREATED="1584581013801" MODIFIED="1584581849662">
+<font BOLD="true"/>
+<node ID="ID_661875853" CREATED="1584581090282" MODIFIED="1584581828226"><richcontent TYPE="NODE">
 
 <html>
   <head>
@@ -100,12 +105,169 @@
   </head>
   <body>
     <p>
-      <b>... or any combination of those</b>
+      <b>only one</b>&nbsp;of these will receive each event
     </p>
   </body>
 </html>
 
 </richcontent>
+</node>
+<node ID="ID_1257678494" CREATED="1584581113088" MODIFIED="1584581960317"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      may be <i>answerful</i>&nbsp;or <i>answerless</i>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Listener" ID="ID_1776670333" CREATED="1584581018392" MODIFIED="1584581851498">
+<font BOLD="true"/>
+<node ID="ID_148388247" CREATED="1584581155083" MODIFIED="1584581834951"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>everyone</b>&nbsp;of these will receive each&nbsp;event
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="Patterns" ID="ID_258737185" CREATED="1584581213734" MODIFIED="1584582254771">
+<font ITALIC="true"/>
+<node TEXT="Publisher / Subscriber" ID="ID_1744298026" CREATED="1584490898041" MODIFIED="1584581853462" HGAP_QUANTITY="15.756097642670499 pt" VSHIFT_QUANTITY="-12.292683498693489 pt"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A.K.A. <b>Producer</b>&nbsp;/ <b>Consumer</b>&nbsp;pattern
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<font BOLD="true"/>
+<node ID="ID_1374141916" CREATED="1584581256068" MODIFIED="1584581945909"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      use the <b>Producer</b>&nbsp;and the <i>answerless</i>&nbsp;<b>Consumer</b>&nbsp;entities
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Broadcast" ID="ID_198716613" CREATED="1584490898041" MODIFIED="1584581856067">
+<font BOLD="true"/>
+<node ID="ID_1719241465" CREATED="1584581480623" MODIFIED="1584581511167"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      use the <b>Producer</b>&nbsp;and <b>Listener</b>&nbsp;entities
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="RFC" ID="ID_1258088003" CREATED="1584498594625" MODIFIED="1584581889096">
+<font BOLD="true"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Remote
+    </p>
+    <p>
+      Function
+    </p>
+    <p>
+      Call
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1399637226" CREATED="1584581899068" MODIFIED="1584581932269"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      use the <b>Producer</b>&nbsp;and the <i>answerful</i>&nbsp;<b>Consumer</b>&nbsp;&nbsp;entities
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Hibrid" ID="ID_1395011358" CREATED="1584499817834" MODIFIED="1584581991354">
+<node ID="ID_263826342" CREATED="1584582004562" MODIFIED="1584582089072"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>RFC</b>&nbsp;+&nbsp;<b>Broadcast</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_873572817" CREATED="1584582046183" MODIFIED="1584582076746"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Publisher</b>&nbsp;/ <b>Subscriber</b>&nbsp;+ <b>Broadcast</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
 </node>
 </node>
 <node TEXT="EventProcessors" POSITION="right" ID="ID_495676614" CREATED="1584490898042" MODIFIED="1584498674576" HGAP_QUANTITY="101.80488213352491 pt" VSHIFT_QUANTITY="-42.14634342409195 pt">
@@ -210,28 +372,181 @@
 </node>
 </node>
 <node TEXT="RingBufferQueue" ID="ID_80153119" CREATED="1584490898048" MODIFIED="1584498350314" HGAP_QUANTITY="15.756097642670499 pt" VSHIFT_QUANTITY="-7.902439392017243 pt"/>
+<node TEXT="LogQueue" ID="ID_937105976" CREATED="1584579816469" MODIFIED="1584579820268">
+<node ID="ID_303898532" CREATED="1584582295584" MODIFIED="1584582308358"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>mmap</b>&nbsp;based
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
 </node>
 </node>
 <node TEXT="DistributedEventChannel" ID="ID_820855915" CREATED="1584498779711" MODIFIED="1584498793142">
 <node TEXT="ZeroMQ based?" ID="ID_1564766141" CREATED="1584498797840" MODIFIED="1584498812179"/>
 </node>
 </node>
-<node TEXT="EventInfo" POSITION="left" ID="ID_25434398" CREATED="1584490898046" MODIFIED="1584499267563" HGAP_QUANTITY="73.70731985079694 pt" VSHIFT_QUANTITY="-37.75609931741573 pt">
+<node TEXT="EventInfo" POSITION="left" ID="ID_25434398" CREATED="1584490898046" MODIFIED="1584580721409" HGAP_QUANTITY="73.70731985079694 pt" VSHIFT_QUANTITY="-37.75609931741573 pt">
 <edge COLOR="#cc00ff"/>
-<node TEXT="RequestInfo" ID="ID_352937383" CREATED="1584490898047" MODIFIED="1584499294837" HGAP_QUANTITY="13.121951178664752 pt" VSHIFT_QUANTITY="-9.658537034687741 pt">
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      or the &quot;Payload&quot;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="RequestInfo" ID="ID_352937383" CREATED="1584490898047" MODIFIED="1584582621198" HGAP_QUANTITY="13.121951178664752 pt" VSHIFT_QUANTITY="-9.658537034687741 pt">
+<font BOLD="true"/>
 <node TEXT="all infos related to the event" ID="ID_668519299" CREATED="1584499542050" MODIFIED="1584499564258"/>
 </node>
-<node TEXT="AnswerInfo" ID="ID_966834988" CREATED="1584490898048" MODIFIED="1584499302406" HGAP_QUANTITY="15.756097642670499 pt" VSHIFT_QUANTITY="-7.902439392017243 pt">
-<node TEXT="the results after the RPC consumers process the &apos;RequestInfo&apos;" ID="ID_1462242212" CREATED="1584499572461" MODIFIED="1584499613897"/>
+<node TEXT="AnswerInfo" ID="ID_966834988" CREATED="1584490898048" MODIFIED="1584582619381" HGAP_QUANTITY="15.756097642670499 pt" VSHIFT_QUANTITY="-7.902439392017243 pt">
+<font BOLD="true"/>
+<node ID="ID_1462242212" CREATED="1584499572461" MODIFIED="1584582404193"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      the results after an answerful&nbsp;<b>consumer</b>&nbsp;(RFC communication pattern) process the 'RequestInfo'
+    </p>
+  </body>
+</html>
+
+</richcontent>
 </node>
-<node TEXT="CommunicationsInfo" ID="ID_1527554640" CREATED="1584499303630" MODIFIED="1584499729186">
-<node TEXT="Control data to allow the synchronization of Pub/Sub, Broadcast, RPC &amp; the several possible thread configurations of the &apos;EventProcessor&apos;" ID="ID_1414875632" CREATED="1584499622839" MODIFIED="1584499740539"/>
 </node>
-<node TEXT="DebugInfo" ID="ID_1497021075" CREATED="1584499331640" MODIFIED="1584499334608">
-<node TEXT="&lt;&lt;get from the exercise&gt;&gt;" ID="ID_16074375" CREATED="1584499753681" MODIFIED="1584499795469"/>
+<node TEXT="CommunicationsInfo" ID="ID_1527554640" CREATED="1584499303630" MODIFIED="1584582617730">
+<font BOLD="true"/>
+<node TEXT="Control data to allow the synchronization of Pub/Sub, Broadcast, RFC &amp; the several possible thread configurations of the &apos;EventProcessor&apos;" ID="ID_1414875632" CREATED="1584499622839" MODIFIED="1584582435291"/>
+<node ID="ID_635014173" CREATED="1584582946341" MODIFIED="1584582961975"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Request<b>CommunicationsInfo</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="{wasConsumed, wasListened}" ID="ID_245030305" CREATED="1584583008582" MODIFIED="1584583011545"/>
+<node TEXT="or, alternatively, just&#xa;{consumedAndListenedCount}" ID="ID_791237945" CREATED="1584583015272" MODIFIED="1584583071192"/>
 </node>
-<node TEXT="MetricsInfo" ID="ID_133500827" CREATED="1584499335865" MODIFIED="1584499345412">
-<node TEXT="&lt;&lt;get from the exercise&gt;&gt;" ID="ID_918246848" CREATED="1584499763627" MODIFIED="1584499773639"/>
+<node ID="ID_1522841277" CREATED="1584582964758" MODIFIED="1584582984734"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Answer<b>CommunicationsInfo</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="{wasAnswerProcessed}" ID="ID_1035038852" CREATED="1584583173472" MODIFIED="1584583176222"/>
+</node>
+</node>
+<node ID="ID_1497021075" CREATED="1584499331640" MODIFIED="1584582612010"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Request<b>DebugInfo</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="{producerId, consumerId} (Ids come from queues debug info)" ID="ID_16074375" CREATED="1584499753681" MODIFIED="1584582772436"/>
+</node>
+<node ID="ID_1069129725" CREATED="1584582592764" MODIFIED="1584582602070"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Answer<b>DebugInfo</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_894691604" CREATED="1584582821666" MODIFIED="1584582840155"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      currently <i>empty</i>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node ID="ID_133500827" CREATED="1584499335865" MODIFIED="1584582635890"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Request<b>MetricsInfo</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="{producerReservationTime, producerPublicationTime, consumerStartTime, consumerEndTime, listeningStartTime, listeningEndTime, slowerListenerElapsedTime, slowerListenerAddr}" ID="ID_918246848" CREATED="1584499763627" MODIFIED="1584582897386"/>
+</node>
+<node ID="ID_1049685516" CREATED="1584582637009" MODIFIED="1584582649648"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Answer<b>MetricsInfo</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="********** FULLFILL **********" ID="ID_1548902716" CREATED="1584583423043" MODIFIED="1584583434281"/>
 </node>
 <node TEXT="Layouts" ID="ID_546298409" CREATED="1584499358556" MODIFIED="1584499368026">
 <font BOLD="true"/>
